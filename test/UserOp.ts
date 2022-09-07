a2ea33a19724955b74e1094acb0fec80aa55debf
Shanshan Feng <shanshan@Shanshans-MacBook-Pro.local>
2022-09-07 11:35:53 +0800

    deposite paymaster and test file

---
+++ test/UserOp.ts
@@ -0,0 +1,233 @@
+import { arrayify, defaultAbiCoder, keccak256 } from "ethers/lib/utils";
+import { BigNumber, Contract, Signer, Wallet } from "ethers";
+import { AddressZero, callDataCost, rethrow } from "./testutils";
+import { ecsign, toRpcSig, keccak256 as keccak256_buffer } from "ethereumjs-util";
+import { EntryPoint } from "../types";
+import { UserOperation } from "./UserOperation";
+
+function encode(typevalues: Array<{ type: string; val: any }>, forSignature: boolean): string {
+  const types = typevalues.map((typevalue) =>
+    typevalue.type === "bytes" && forSignature ? "bytes32" : typevalue.type,
+  );
+  const values = typevalues.map((typevalue) =>
+    typevalue.type === "bytes" && forSignature ? keccak256(typevalue.val) : typevalue.val,
+  );
+  return defaultAbiCoder.encode(types, values);
+}
+
+// export function packUserOp(op: UserOperation, hashBytes = true): string {
+//   if ( !hashBytes || true ) {
+//     return packUserOp1(op, hashBytes)
+//   }
+//
+//   const opEncoding = Object.values(testUtil.interface.functions).find(func => func.name == 'packUserOp')!.inputs[0]
+//   let packed = defaultAbiCoder.encode([opEncoding], [{...op, signature:'0x'}])
+//   packed = '0x'+packed.slice(64+2) //skip first dword (length)
+//   packed = packed.slice(0,packed.length-64) //remove signature (the zero-length)
+//   return packed
+// }
+
+export function packUserOp(op: UserOperation, forSignature = true): string {
+  if (forSignature) {
+    // lighter signature scheme (must match UserOperation#pack): do encode a zero-length signature, but strip afterwards the appended zero-length value
+    const userOpType = {
+      components: [
+        { type: "address", name: "sender" },
+        { type: "uint256", name: "nonce" },
+        { type: "bytes", name: "initCode" },
+        { type: "bytes", name: "callData" },
+        { type: "uint256", name: "callGas" },
+        { type: "uint256", name: "verificationGas" },
+        { type: "uint256", name: "preVerificationGas" },
+        { type: "uint256", name: "maxFeePerGas" },
+        { type: "uint256", name: "maxPriorityFeePerGas" },
+        { type: "address", name: "paymaster" },
+        { type: "bytes", name: "paymasterData" },
+        { type: "bytes", name: "signature" },
+      ],
+      name: "userOp",
+      type: "tuple",
+    };
+    let encoded = defaultAbiCoder.encode([userOpType as any], [{ ...op, signature: "0x" }]);
+    // remove leading word (total length) and trailing word (zero-length signature)
+    encoded = "0x" + encoded.slice(66, encoded.length - 64);
+    return encoded;
+  }
+  const typevalues = [
+    { type: "address", val: op.sender },
+    { type: "uint256", val: op.nonce },
+    { type: "bytes", val: op.initCode },
+    { type: "bytes", val: op.callData },
+    { type: "uint256", val: op.callGas },
+    { type: "uint256", val: op.verificationGas },
+    { type: "uint256", val: op.preVerificationGas },
+    { type: "uint256", val: op.maxFeePerGas },
+    { type: "uint256", val: op.maxPriorityFeePerGas },
+    { type: "address", val: op.paymaster },
+    { type: "bytes", val: op.paymasterData },
+  ];
+  if (!forSignature) {
+    // for the purpose of calculating gas cost, also hash signature
+    typevalues.push({ type: "bytes", val: op.signature });
+  }
+  return encode(typevalues, forSignature);
+}
+
+export function packUserOp1(op: UserOperation): string {
+  return defaultAbiCoder.encode(
+    [
+      "address", // sender
+      "uint256", // nonce
+      "bytes32", // initCode
+      "bytes32", // callData
+      "uint256", // callGas
+      "uint", // verificationGas
+      "uint", // preVerificationGas
+      "uint256", // maxFeePerGas
+      "uint256", // maxPriorityFeePerGas
+      "address", // paymaster
+      "bytes32", // paymasterData
+    ],
+    [
+      op.sender,
+      op.nonce,
+      keccak256(op.initCode),
+      keccak256(op.callData),
+      op.callGas,
+      op.verificationGas,
+      op.preVerificationGas,
+      op.maxFeePerGas,
+      op.maxPriorityFeePerGas,
+      op.paymaster,
+      keccak256(op.paymasterData),
+    ],
+  );
+}
+
+export function getRequestId(op: UserOperation, entryPoint: string, chainId: number): string {
+  const userOpHash = keccak256(packUserOp(op, true));
+  const enc = defaultAbiCoder.encode(["bytes32", "address", "uint256"], [userOpHash, entryPoint, chainId]);
+  return keccak256(enc);
+}
+
+export const DefaultsForUserOp: UserOperation = {
+  sender: AddressZero,
+  nonce: 0,
+  initCode: "0x",
+  callData: "0x",
+  callGas: 0,
+  verificationGas: 100000, // default verification gas. will add create2 cost (3200+200*length) if initCode exists
+  preVerificationGas: 21000, // should also cover calldata cost.
+  maxFeePerGas: 0,
+  maxPriorityFeePerGas: 1e9,
+  paymaster: AddressZero,
+  paymasterData: "0x",
+  signature: "0x",
+};
+
+export function signUserOp(op: UserOperation, signer: Wallet, entryPoint: string, chainId: number): UserOperation {
+  const message = getRequestId(op, entryPoint, chainId);
+  const msg1 = Buffer.concat([
+    Buffer.from("\x19Ethereum Signed Message:\n32", "ascii"),
+    Buffer.from(arrayify(message)),
+  ]);
+
+  const sig = ecsign(keccak256_buffer(msg1), Buffer.from(arrayify(signer.privateKey)));
+  // that's equivalent of:  await signer.signMessage(message);
+  // (but without "async"
+  const signedMessage1 = toRpcSig(sig.v, sig.r, sig.s);
+  return {
+    ...op,
+    signature: signedMessage1,
+  };
+}
+
+export function fillUserOp(op: Partial<UserOperation>, defaults = DefaultsForUserOp): UserOperation {
+  const partial: any = { ...op };
+  // we want "item:undefined" to be used from defaults, and not override defaults, so we must explicitly
+  // remove those so "merge" will succeed.
+  for (const key in partial) {
+    if (partial[key] == null) {
+      // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+      delete partial[key];
+    }
+  }
+  const filled = { ...defaults, ...partial };
+  return filled;
+}
+
+// helper to fill structure:
+// - default callGas to estimate call from entryPoint to wallet (TODO: add overhead)
+// if there is initCode:
+//  - default nonce (used as salt) to zero
+//  - calculate sender using getSenderAddress
+//  - default verificationGas to create2 cost + 100000
+// no initCode:
+//  - update nonce from wallet.nonce()
+// entryPoint param is only required to fill in "sender address when specifying "initCode"
+// nonce: assume contract as "nonce()" function, and fill in.
+// sender - only in case of construction: fill sender from initCode.
+// callGas: VERY crude estimation (by estimating call to wallet, and add rough entryPoint overhead
+// verificationGas: hard-code default at 100k. should add "create2" cost
+export async function fillAndSign(
+  op: Partial<UserOperation>,
+  signer: Wallet | Signer,
+  entryPoint?: EntryPoint,
+): Promise<UserOperation> {
+  const op1 = { ...op };
+  const provider = entryPoint?.provider;
+  if (op.initCode != null) {
+    // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+    if (!op1.nonce) op1.nonce = 0;
+    if (op1.sender == null) {
+      if (entryPoint == null) throw new Error("must have entryPoint to calc sender address from initCode");
+      op1.sender = await entryPoint.getSenderAddress(op.initCode, op1.nonce);
+    }
+    if (op1.verificationGas == null) {
+      op1.verificationGas = BigNumber.from(DefaultsForUserOp.verificationGas).add(
+        32000 + (200 * op.initCode.length) / 2,
+      );
+    }
+  }
+  if (op1.nonce == null) {
+    if (provider == null) throw new Error("must have entryPoint to autofill nonce");
+    const c = new Contract(op.sender!, ["function nonce() view returns(address)"], provider);
+    op1.nonce = await c.nonce().catch(rethrow());
+  }
+  if (op1.callGas == null && op.callData != null) {
+    if (provider == null) throw new Error("must have entryPoint for callGas estimate");
+    const gasEtimated = await provider.estimateGas({
+      from: entryPoint?.address,
+      to: op1.sender,
+      data: op1.callData,
+    });
+
+    // console.log('estim', op1.sender,'len=', op1.callData!.length, 'res=', gasEtimated)
+    // estimateGas assumes direct call from entryPoint. add wrapper cost.
+    op1.callGas = gasEtimated; // .add(55000)
+  }
+  if (op1.maxFeePerGas == null) {
+    if (provider == null) throw new Error("must have entryPoint to autofill maxFeePerGas");
+    const block = await provider.getBlock("latest");
+    op1.maxFeePerGas = block.baseFeePerGas!.add(op1.maxPriorityFeePerGas ?? DefaultsForUserOp.maxPriorityFeePerGas);
+  }
+  // TODO: this is exactly what fillUserOp below should do - but it doesn't.
+  // adding this manually
+  if (op1.maxPriorityFeePerGas == null) {
+    op1.maxPriorityFeePerGas = DefaultsForUserOp.maxPriorityFeePerGas;
+  }
+  const op2 = fillUserOp(op1);
+  // eslint-disable-next-line @typescript-eslint/no-base-to-string
+  if (op2.preVerificationGas.toString() === "0") {
+    // TODO: we don't add overhead, which is ~21000 for a single TX, but much lower in a batch.
+    op2.preVerificationGas = callDataCost(packUserOp(op2, false));
+  }
+
+  const chainId = await provider!.getNetwork().then((net) => net.chainId);
+  const message = arrayify(getRequestId(op2, entryPoint!.address, chainId));
+
+  return {
+    ...op2,
+    signature: await signer.signMessage(message),
+  };
+}
---
+++ test/testDepositPaymaster.ts
@@ -0,0 +1,255 @@
+import "./aa.init";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import {
+  SimpleWallet,
+  SimpleWallet__factory,
+  EntryPoint,
+  DepositPaymaster,
+  DepositPaymaster__factory,
+  MaskToken,
+  MaskToken__factory,
+  TestCounter,
+  TestCounter__factory,
+  HappyRedPacket,
+  HappyRedPacket__factory,
+} from "../types";
+//const { deployContract } = waffle;
+
+import { AddressZero, createAddress, createWalletOwner, deployEntryPoint, FIVE_ETH, ONE_ETH } from "./testutils";
+import { fillAndSign } from "./UserOp";
+import { hexZeroPad, parseEther } from "ethers/lib/utils";
+
+describe("DepositPaymaster", () => {
+  let entryPoint: EntryPoint;
+  let entryPointStatic: EntryPoint;
+  const ethersSigner = ethers.provider.getSigner();
+  let maskToken: MaskToken;
+  let paymaster: DepositPaymaster;
+  let redpacket: HappyRedPacket;
+
+  before(async function () {
+    entryPoint = await deployEntryPoint(1, 1);
+    entryPointStatic = entryPoint.connect(AddressZero);
+    maskToken = await new MaskToken__factory(ethersSigner).deploy();
+
+    paymaster = await new DepositPaymaster__factory(ethersSigner).deploy(entryPoint.address, maskToken.address);
+    await paymaster.addStake(0, { value: parseEther("2") });
+    await entryPoint.depositTo(paymaster.address, { value: parseEther("1") });
+
+    await maskToken.mint(await ethersSigner.getAddress(), FIVE_ETH);
+    await maskToken.approve(paymaster.address, ethers.constants.MaxUint256);
+  });
+
+  describe("deposit and withdraw", () => {
+    let wallet: SimpleWallet;
+
+    before(async () => {
+      wallet = await new SimpleWallet__factory(ethersSigner).deploy(
+        entryPoint.address,
+        await ethersSigner.getAddress(),
+      );
+    });
+    it("should deposit and read balance", async () => {
+      await paymaster.addDepositFor(wallet.address, 100);
+      expect(await paymaster.depositInfo(wallet.address)).to.eql({ amount: 100 });
+    });
+    it("should fail to withdraw without unlock", async () => {
+      const paymasterWithdraw = await paymaster.populateTransaction
+        .withdrawTokensTo(AddressZero, 1)
+        .then((tx) => tx.data!);
+
+      await expect(wallet.exec(paymaster.address, 0, paymasterWithdraw)).to.revertedWith(
+        "DepositPaymaster: must unlockTokenDeposit",
+      );
+    });
+    it("should fail to withdraw within the same block ", async () => {
+      const paymasterUnlock = await paymaster.populateTransaction.unlockTokenDeposit().then((tx) => tx.data!);
+      const paymasterWithdraw = await paymaster.populateTransaction
+        .withdrawTokensTo(AddressZero, 1)
+        .then((tx) => tx.data!);
+
+      await expect(
+        wallet.execBatch([paymaster.address, paymaster.address], [paymasterUnlock, paymasterWithdraw]),
+      ).to.be.revertedWith("DepositPaymaster: must unlockTokenDeposit");
+    });
+    it("should succeed to withdraw after unlock", async () => {
+      const paymasterUnlock = await paymaster.populateTransaction.unlockTokenDeposit().then((tx) => tx.data!);
+      const target = createAddress();
+      const paymasterWithdraw = await paymaster.populateTransaction.withdrawTokensTo(target, 1).then((tx) => tx.data!);
+      await wallet.exec(paymaster.address, 0, paymasterUnlock);
+      await wallet.exec(paymaster.address, 0, paymasterWithdraw);
+      expect(await maskToken.balanceOf(target)).to.eq(1);
+    });
+  });
+
+  describe("#validatePaymasterUserOp", () => {
+    let wallet: SimpleWallet;
+    const gasPrice = 1e9;
+    let walletOwner: string;
+
+    before(async () => {
+      walletOwner = await ethersSigner.getAddress();
+      wallet = await new SimpleWallet__factory(ethersSigner).deploy(entryPoint.address, walletOwner);
+    });
+
+    it("should fail if no token", async () => {
+      const userOp = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+        },
+        ethersSigner,
+        entryPoint,
+      );
+      await expect(entryPointStatic.callStatic.simulateValidation(userOp)).to.be.revertedWith(
+        "paymasterData must specify token",
+      );
+    });
+
+    it("should reject if no deposit", async () => {
+      const userOp = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+          paymasterData: hexZeroPad(maskToken.address, 32),
+        },
+        ethersSigner,
+        entryPoint,
+      );
+      await expect(entryPointStatic.callStatic.simulateValidation(userOp, { gasPrice })).to.be.revertedWith(
+        "DepositPaymaster: deposit too low",
+      );
+    });
+
+    it("should reject if deposit is not locked", async () => {
+      await paymaster.addDepositFor(wallet.address, ONE_ETH);
+
+      const paymasterUnlock = await paymaster.populateTransaction.unlockTokenDeposit().then((tx) => tx.data!);
+      await wallet.exec(paymaster.address, 0, paymasterUnlock);
+
+      const userOp = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+          paymasterData: hexZeroPad(maskToken.address, 32),
+        },
+        ethersSigner,
+        entryPoint,
+      );
+      await expect(entryPointStatic.callStatic.simulateValidation(userOp, { gasPrice })).to.be.revertedWith(
+        "not locked",
+      );
+    });
+
+    it("succeed with valid deposit", async () => {
+      // needed only if previous test did unlock.
+      const paymasterLockTokenDeposit = await paymaster.populateTransaction.lockTokenDeposit().then((tx) => tx.data!);
+      await wallet.exec(paymaster.address, 0, paymasterLockTokenDeposit);
+
+      const userOp = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+          paymasterData: hexZeroPad(maskToken.address, 32),
+        },
+        ethersSigner,
+        entryPoint,
+      );
+      await entryPointStatic.callStatic.simulateValidation(userOp);
+    });
+  });
+
+  describe("#handleOps", () => {
+    let wallet: SimpleWallet;
+    const walletOwner = createWalletOwner();
+    let counter: TestCounter;
+    let callData: string;
+    let callData2: string;
+    before(async () => {
+      wallet = await new SimpleWallet__factory(ethersSigner).deploy(entryPoint.address, walletOwner.address);
+      counter = await new TestCounter__factory(ethersSigner).deploy();
+      const counterJustEmit = await counter.populateTransaction.justemit().then((tx) => tx.data!);
+      callData = await wallet.populateTransaction
+        .execFromEntryPoint(counter.address, 0, counterJustEmit)
+        .then((tx) => tx.data!);
+      callData2 = await wallet.populateTransaction
+        .execFromEntryPoint(counter.address, 1, counterJustEmit)
+        .then((tx) => tx.data!);
+
+      await paymaster.addDepositFor(wallet.address, ONE_ETH);
+    });
+    it("should pay with deposit (and revert user's call) if user can't pay with maskTokens", async () => {
+      const beneficiary = createAddress();
+      const userOp = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+          paymasterData: hexZeroPad(maskToken.address, 32),
+          callData,
+        },
+        walletOwner,
+        entryPoint,
+      );
+
+      await entryPoint.handleOps([userOp], beneficiary);
+
+      const [log] = await entryPoint.queryFilter(entryPoint.filters.UserOperationEvent());
+      expect(log.args.success).to.eq(false);
+      expect(await counter.queryFilter(counter.filters.CalledFrom())).to.eql([]);
+      expect(await ethers.provider.getBalance(beneficiary)).to.be.gt(0);
+    });
+
+    it("should pay with tokens if available", async () => {
+      const beneficiary = createAddress();
+      const beneficiary1 = createAddress();
+      const initialTokens = parseEther("1");
+      await maskToken.mint(wallet.address, initialTokens);
+      await paymaster.setMaskToEthRadio(2000);
+      console.log("before balance", await maskToken.balanceOf(wallet.address));
+
+      // need to "approve" the paymaster to use the tokens. we issue a UserOp for that (which uses the deposit to execute)
+      const tokenApprovePaymaster = await maskToken.populateTransaction
+        .approve(paymaster.address, ethers.constants.MaxUint256)
+        .then((tx) => tx.data!);
+      const execApprove = await wallet.populateTransaction
+        .execFromEntryPoint(maskToken.address, 0, tokenApprovePaymaster)
+        .then((tx) => tx.data!);
+      const userOp1 = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+          paymasterData: hexZeroPad(maskToken.address, 32),
+          callData: execApprove,
+        },
+        walletOwner,
+        entryPoint,
+      );
+      await entryPoint.handleOps([userOp1], beneficiary1);
+
+      const userOp = await fillAndSign(
+        {
+          sender: wallet.address,
+          paymaster: paymaster.address,
+          paymasterData: hexZeroPad(maskToken.address, 32),
+          callData,
+        },
+        walletOwner,
+        entryPoint,
+      );
+      await entryPoint.handleOps([userOp], beneficiary);
+
+      const [log] = await entryPoint.queryFilter(
+        entryPoint.filters.UserOperationEvent(),
+        await ethers.provider.getBlockNumber(),
+      );
+      expect(log.args.success).to.eq(true);
+      const charge = log.args.actualGasCost;
+      expect(await ethers.provider.getBalance(beneficiary)).to.eq(charge);
+
+      const targetLogs = await counter.queryFilter(counter.filters.CalledFrom());
+      expect(targetLogs.length).to.eq(1);
+      console.log("after balance", await maskToken.balanceOf(wallet.address));
+    });
+  });
+});
